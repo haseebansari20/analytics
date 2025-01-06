a57134789519b58cfb106eb5ae421e2834a7b3d8
Haseeb Ahmed Ansari <haseebahmedansari20@gmail.com>
2025-01-06 12:21:38 +0500

    GA id changed for testing

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -35,9 +35,10 @@ export default function RootLayout({ children }) {
         {/* <GoogleAnalytics />
         <MyForm /> */}
         {/* <Navbar />
+
         <Card/> */}
         <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=G-H86EYVTC69`}
+        src={`https://www.googletagmanager.com/gtag/js?id=G-E9KSFS3GG8`}
         strategy="afterInteractive"
       />
       <Script id="google-analytics" strategy="afterInteractive">
@@ -45,7 +46,7 @@ export default function RootLayout({ children }) {
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'G-H86EYVTC69');
+          gtag('config', 'G-E9KSFS3GG8');
         `}
       </Script>
         {children}
